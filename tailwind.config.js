54cda958ffe947c9a165719bf98b82440f7f4e7d
lutfi oktovian <lutfioktovian28@gmail.com>
2022-05-08 16:54:15 +0700

    08/05/2022

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily : {
+        amiri : ['Amiri'],
+        poppins : ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
